f59aacc0f587661d4e5a557992f5ecfc40571137
Grant Garstang <Grant Garstang>
2017-02-15 17:08:23 +0000

    completed Jquery table challenge

---
+++ JQuery/Sample Site/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+	$(document).ready(function() {
+		$("h2").css("text-decoration", "underline");
+		$("ul").css("border", "solid 1px #ccc");
+	});
+
+
+}); 
